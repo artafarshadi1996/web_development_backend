077d8cef3e1754a56d07800c5f09e294bdc65d96
Arta Farashadi <artafarshadi1996@gmail.com>
2021-04-12 03:42:57 +0000

    employee getrequest add err checking

--- routes/index.js
+++ routes/index.js
@@ -26,11 +26,19 @@ router.post('/',(request,response) => {
 });
 
 router.get('/employees/:employeeId/', async (req,res,next)=> {
-  console.log('employeeId', req.params.employeeId);  // gives :uid
-   
+  const employeeId = parseInt(req.params.employeeId);
+  console.log('employeeId', employeeId);  // gives :uid
+  if (isNaN(employeeId) || (employeeId<0)) {
+    return res.sendStatus(400);
+  }
+
   try {
-    let results = await db.getEmployeeId(req.params.employeeId);
-    res.json(results);
+    let results = await db.getEmployeeId(employeeId);
+    if (results.length == 0){
+      res.sendStatus(404);
+    } else {
+      res.json(results);
+    }   
   } catch(e) {
     console.log(e);
     res.sendStatus(500);
