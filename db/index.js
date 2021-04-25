f2a33ed12f5ce59450f6f16a172d152fa5e74e05
Arta Farashadi <artafarshadi1996@gmail.com>
2021-04-25 20:29:55 +0000

    Changes for timecards/add path and timecards total hours and start and end on db and routes

--- db/index.js
+++ db/index.js
@@ -22,18 +22,10 @@ const mysql = require ('mysql');
 
 let chirprdb = {}; 
 
-chirprdb.all = () => {
-  return new Promise ((resolve, reject) => {
-    pool.query(`SELECT * FROM Question`, (err,results) =>{
-      if(err){
-        return reject(err);
-      }
-      return resolve(results);
-    });
-  });
-};
-
 chirprdb.getEmployeeId = (id) => {
+    ///*grabs all the columns from the table
+
+  
   return new Promise ((resolve, reject) => {
     pool.query(`SELECT * FROM Employees where id=? `, id, (err,results) =>{
       if(err){
@@ -44,5 +36,265 @@ chirprdb.getEmployeeId = (id) => {
   });
 };
 
+
+chirprdb.addEmployee = (JsonRequest) => {
+
+  const id = JsonRequest.id;
+  const name = JsonRequest.name;
+  return new Promise ((resolve, reject) => {
+    let sqlInsert;
+    let sqlValues;
+
+    if (id === undefined){
+      sqlInsert = `INSERT INTO Employees (names) VALUES (?)`;
+      sqlValues =  [name];
+    } else {
+      sqlInsert = `INSERT INTO Employees (id,names) VALUES (?,?)` ;
+      sqlValues = [id, name];
+    }
+
+    pool.query(sqlInsert, sqlValues, (err,results) =>{
+      if(err){
+        return reject(err);
+      }
+      return resolve(results);
+    });
+  });
+};
+
 module.exports = chirprdb;
+
+
+
+//delete method
+chirprdb.deleteEmployeeId = (id) => {
+
+  
+  return new Promise ((resolve, reject) => {
+    pool.query(`DELETE FROM Employees where id=? `, id, (err,results) =>{
+      if(err){
+        return reject(err);
+      }
+      return resolve(results);
+    });
+  });
+}
+
+
+///update
+
+
+chirprdb.updateEmployee = (JsonRequest, employeeId) => {
+
+  const id = employeeId;
+  const name = JsonRequest.name;
+  return new Promise ((resolve, reject) => {
+    let sqlUpdate;
+    let sqlValues;
+     
+    
+   sqlUpdate = `UPDATE Employees SET Names=? WHERE id=?`;
+   sqlValues =  [name, id];
+
+     
+
+    pool.query(sqlUpdate, sqlValues, (err,results) =>{
+      if(err){
+        return reject(err);
+      }
+      return resolve(results);
+    });
+  });
+};
+
+
+/// Department
+
+chirprdb.getdepartmentById = (id) => {
+    ///*grabs all the columns from the table
+
+  
+  return new Promise ((resolve, reject) => {
+    pool.query(`SELECT * FROM department where id=? `, id, (err,results) =>{
+      if(err){
+        return reject(err);
+      }
+      return resolve(results);
+    });
+  });
+};
+
+
+chirprdb.adddepartment = (JsonRequest) => {
+
+  const id = JsonRequest.id;
+  const name = JsonRequest.name;
+  return new Promise ((resolve, reject) => {
+    let sqlInsert;
+    let sqlValues;
+
+    if (id === undefined){
+      sqlInsert = `INSERT INTO department (Names) VALUES (?)`;
+      sqlValues =  [name];
+    } else {
+      sqlInsert = `INSERT INTO department (id,Names) VALUES (?,?)` ;
+      sqlValues = [id, name];
+    }
+
+    pool.query(sqlInsert, sqlValues, (err,results) =>{
+      if(err){
+        return reject(err);
+      }
+      return resolve(results);
+    });
+  });
+};
+
+module.exports = chirprdb;
+
+
+
+//delete method
+chirprdb.deletedepartmentId = (id) => {
+
+  
+  return new Promise ((resolve, reject) => {
+    pool.query(`DELETE FROM department where id=? `, id, (err,results) =>{
+      if(err){
+        return reject(err);
+      }
+      return resolve(results);
+    });
+  });
+}
+
+
+///update
+
+
+chirprdb.updatedepartments = (JsonRequest, departmentId) => {
+
+  const id = departmentId;
+  const name = JsonRequest.name;
+  return new Promise ((resolve, reject) => {
+    let sqlUpdate;
+    let sqlValues;
+     
+    
+   sqlUpdate = `UPDATE department SET Names=? WHERE id=?`;
+   sqlValues =  [name, id];
+
+     
+
+    pool.query(sqlUpdate, sqlValues, (err,results) =>{
+      if(err){
+        return reject(err);
+      }
+      return resolve(results);
+    });
+  });
+};
+
+///ADD timecards
+chirprdb.addtimecards = (JsonRequest) => {
+
+  const id = JsonRequest.id;
+  const employeeId =JsonRequest.employeesid;
+  const departmentId = JsonRequest.departmentid;
+  const date = JsonRequest.date;
+  const hours = JsonRequest.hours;
+  
+
+
+
+
+
+
+  return new Promise ((resolve, reject) => {
+    let sqlInsert;
+    let sqlValues;
+    ///id	employeesid	Hours	departmentid	Date	
+    if (id === undefined){
+      sqlInsert = `INSERT INTO workingHours (employeesid, Hours, departmentid, Date) VALUES (?,?,?,?)`;
+      sqlValues =  [employeeId, hours, departmentId, date];
+    } else {
+      sqlInsert = `INSERT INTO workingHours (id,employeesid, Hours, departmentid, Date) VALUES (?,?,?,?,?)` ;
+      sqlValues = [id,employeeId, hours, departmentId, date];
+    }
+
+    pool.query(sqlInsert, sqlValues, (err,results) =>{
+      if(err){
+        return reject(err);
+      }
+      return resolve(results);
+    });
+  });
+};
+
+chirprdb.gettimecardsid = (id) => {
+  ///*grabs all the columns from the table
+
+return new Promise ((resolve, reject) => {
+  pool.query(`SELECT * FROM workingHours where id=? `, id, (err,results) =>{
+    if(err){
+      return reject(err);
+    }
+    return resolve(results);
+  });
+});
+};
+
+chirprdb.deletetimecardsid = (id) => {
+
+  
+  return new Promise ((resolve, reject) => {
+    pool.query(`DELETE FROM workingHours where id=? `, id, (err,results) =>{
+      if(err){
+        return reject(err);
+      }
+      return resolve(results);
+    });
+  });
+}
+
+
+chirprdb.totaltimecardsforaemployee = (id,start,end) => {
+  return new Promise ((resolve, reject) => {
+    pool.query(`SELECT * FROM workingHours WHERE employeesid=? and Date BETWEEN ? AND ?`, [id,start, end], (err,results) =>{
+      if(err){
+        return reject(err);
+      }
+      return resolve(results);
+    });
+  });
+}
+
+
+chirprdb.sumofthetotalhoursforemployee = (id, start, end) => {
+  return new Promise ((resolve, reject) => {
+    pool.query(`SELECT SUM(Hours) as "total" FROM workingHours WHERE employeesid=? and Date BETWEEN ? AND ?`, [id, start, end], (err,results) =>{
+      if(err){
+        return reject(err);
+      }
+      return resolve(results);
+    });
+  });
+}
+
+
+///start and end
+
+
+
+/*chirprdb.gettimecardhours = (id,startTime,endTime) =>{
+  return new Promise ((resolve, reject) => {
+    pool.query(`SELECT SUM(Hours) FROM workingHours WHERE employeesid=? and Date BETWEEN ? AND ?`, [id,startTime,endTime], (err,results) =>{
+      if(err){
+        return reject(err);
+      }
+      return resolve(results);
+    });
+  });
+}*/
+
 
--- routes/index.js
+++ routes/index.js
@@ -1,53 +1,384 @@
-
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 
+function validateid(id, res, checknumber) {
+  const intId = parseInt(id);
+  console.log('employeeId', intId);  // gives :uid
+  if (isNaN(intId) || (intId<0)) {
+    if (checknumber){
+      res.sendStatus(400);
+    }
+    
+    return null;
+  }
+
+  return intId; 
+}
 
 const express =require('express');
 const db =require('../db');
+const { getEmployeeId } = require('../db');
 const router = express.Router();
 
-/*
-router.get('/',async (req, res, next)=> {
-  try {
-    let results = await db.all();
+
+router.post('/employees/add',async(req,res) => {
+  let json_object = req.body;
+  const employeeId = validateid(json_object.id, res, json_object.id !== undefined);
+
+  console.log(json_object);
+
+  try {  //gives the dictionarey to my db functions
+    let results = await db.addEmployee(json_object)
+    .then((results) => {
+      const insertId = results.insertId;
+      return db.getEmployeeId(insertId);
+    })
+
+    
+    // res.json(results);
     res.json(results);
-  } catch(e) {
+    
+  } catch(e) {  
+    if (e.code = "ER_DUP_ENTRY"){
+      console.log(e);
+      res.sendStatus(400);
+      return;
+    }
+
     console.log(e);
     res.sendStatus(500);
   }
 });
-*/
 
-router.post('/',(request,response) => {
-    //code to perform particular action.
-    //To access POST variable use req.body()methods.
-    response.send(request.body);
-});
 
 router.get('/employees/:employeeId/', async (req,res,next)=> {
-  const employeeId = parseInt(req.params.employeeId);
-  console.log('employeeId', employeeId);  // gives :uid
-  if (isNaN(employeeId) || (employeeId<0)) {
-    return res.sendStatus(400);
+  const employeeId = validateid(req.params.employeeId, res, true);
+  if (employeeId != null){
+    try {
+      let results = await db.getEmployeeId(employeeId);
+      if (results.length == 0){
+        res.sendStatus(404);
+      } else {
+        res.json(results);
+      }   
+    } catch(e) {
+      console.log(e);
+      res.sendStatus(500);
+    }
   }
+});
 
+
+//delete req
+//make dp function that can delete id
+router.delete('/employees/:employeeId/', async (req,res,next)=> {
+  const employeeId = validateid(req.params.employeeId, res, true);
+
+  // if employeeId == null ... error
+
+  if (employeeId == null){
+    res.sendStatus(400);
+    return;
+  }
+  
   try {
-    let results = await db.getEmployeeId(employeeId);
-    if (results.length == 0){
+    let results = await db.deleteEmployeeId(employeeId);
+    console.log(results);
+    if (results.affectedRows == 0){
+  
       res.sendStatus(404);
     } else {
-      res.json(results);
+      res.sendStatus(200);
     }   
   } catch(e) {
     console.log(e);
     res.sendStatus(500);
   }
 
+
+});
+
+//PUT update
+// create function to update the values  pars through the json
+
+router.put('/employees/:employeeId/',async(req,res) => {
+  let json_object = req.body;
+  const employeeId = validateid(req.params.employeeId, res, true);
+  
+  console.log(json_object);
+  try {  //gives the dictionarey to my db functions
+    let results = await db.updateEmployee(json_object,employeeId)
+    .then((results) => {
+
+    console.log(results);
+
+      
+      return db.getEmployeeId(employeeId);
+    })
+
+    res.json(results);
+  } catch(e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
+  
+});
+
+
+router.post('/employees/:employeeId/timecard', async (req,res,next)=> {
+  let json_object = req.body;  
+  const employeeId = validateid(req.params.employeeId, res, true);
+  const start = Date.parse(json_object.start);
+  if (isNaN(start)){
+    res.sendStatus(400);
+    return;
+  }
+  const end = Date.parse(json_object.end);
+  if (isNaN(end)){
+    res.sendStatus(400);
+    return;
+  }
+  //console.log(start)
+  //console.log(end)
+
+  try {
+    let enteryresults = await db.totaltimecardsforaemployee(employeeId,json_object.start,json_object.end);
+    let totalhours = await db.sumofthetotalhoursforemployee(employeeId,json_object.start,json_object.end);
+    let variable = {
+      "enteries": enteryresults, 
+      "total":totalhours[0].total,
+    }
+
+    res.json(variable);
+  }  catch(e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
+
+
+  
+ /* let varaible = [
+    "Cat",
+    "Dog",
+    "Bunny"
+  ];*/
+
 });
 
 
 
+  // department 
+
+  ///POST
+  router.post('/departments/add',async(req,res) => {
+  let json_object = req.body;
+  const departmentId = validateid(json_object.id, res, json_object.id !== undefined);
+
+  console.log(json_object);
+
+  try {  //gives the dictionarey to my db functions
+    let results = await db.adddepartment(json_object)
+    .then((results) => {
+      const insertId = results.insertId;
+      return db.getdepartmentById(insertId);
+    })
+
+    
+    // res.json(results);
+    res.json(results);
+    
+  } catch(e) {  
+    if (e.code = "ER_DUP_ENTRY"){
+      console.log(e);
+      res.sendStatus(400);
+      return;
+    }
+
+    console.log(e);
+    res.sendStatus(500);
+  }
+});
+
+///GET
+router.get('/departments/:departmentId/', async (req,res,next)=> {
+  const departmentId = validateid(req.params.departmentId, res, true);
+  if (departmentId != null){
+    try {
+      let results = await db.getdepartmentById(departmentId);
+      if (results.length == 0){
+        res.sendStatus(404);
+      } else {
+        res.json(results);
+      }   
+    } catch(e) {
+      console.log(e);
+      res.sendStatus(500);
+    }
+  }
+});
+
+
+//delete req
+//make dp function that can delete id
+router.delete('/departments/:departmentId/', async (req,res,next)=> {
+  const departmentId = validateid(req.params.departmentId, res, true);
+
+  // if employeeId == null ... error
+
+  if (departmentId == null){
+    res.sendStatus(400);
+    return;
+  }
+  
+  try {
+    let results = await db.deletedepartmentId(departmentId);
+    console.log(results);
+    if (results.affectedRows == 0){
+  
+      res.sendStatus(404);
+    } else {
+      res.sendStatus(200);
+    }   
+  } catch(e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
+
+
+});
+
+//PUT update
+// create function to update the values  pars through the json
+
+router.put('/departments/:departmentId/',async(req,res) => {
+  let json_object = req.body;
+  const departmentId = validateid(req.params.departmentId, res, true);
+  
+  console.log(json_object);
+  try {  //gives the dictionarey to my db functions
+    let results = await db.updatedepartments(json_object,departmentId)
+    .then((results) => {
+
+    console.log(results);
+
+      
+      return db.getdepartmentById(departmentId);
+    })
+
+    res.json(results);
+  } catch(e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
+  
+});
+
+  
+
+////Add and Delete timecards
+router.post('/timecards/add',async(req,res) => {
+  let json_object = req.body;
+  const timecardid = validateid(json_object.id, res, json_object.id !== undefined);
+  const employeeId = validateid(json_object.employeesid, res, true);
+  const departmentId = validateid(json_object.departmentid, res, true);
+  const hours = parseFloat(json_object.hours);
+  if (isNaN(hours) || (hours>24) || (hours<0)) {
+    res.sendStatus(400);
+    return;
+  }
+  console.log(json_object);
+  const date = Date.parse(json_object.date);
+  if (isNaN(date)){
+
+    res.sendStatus(400);
+    return;
+  }
+
+  try {  //gives the dictionarey to my db functions
+    let results = await db.addtimecards(json_object)
+    .then((results) => {
+      const insertId = results.insertId;
+      return db.gettimecardsid(insertId);
+    })
+
+    
+    // res.json(results);
+    res.json(results);
+    
+  } catch(e) {  
+    if (e.code = "ER_DUP_ENTRY"){
+      console.log(e);
+      res.sendStatus(400);
+      return;
+    }
+
+    console.log(e);
+    res.sendStatus(500);
+  } 
+});
+
+
+
+//delete timecard
+router.delete('/timecards/:timecardId', async (req,res,next)=> {
+  const timecardid = validateid(req.params.timecardId, res, true);
+
+  // if employeeId == null ... error
+  if (timecardid == null){
+    res.sendStatus(400);
+    return;
+  }
+  
+  try {
+    let results = await db.deletetimecardsid(timecardid);
+    console.log(results);
+    if (results.affectedRows == 0){
+      res.sendStatus(404);
+    } else {
+      res.sendStatus(200);
+    }   
+  } catch(e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
+});
+
+//////Timecardrequest
+
+/*router.post('employees/:employeeid/timecard', async(req,res) => {
+  let json_object = req.body;
+  //console.log(json_object);
+  const employeeId = validateid(req.params.employeesid, res, true);
+  const start = Date.parse(json_object.start);
+  if (isNaN(start)){
+    res.sendStatus(400);
+    return;
+  }
+  const end = Date.parse(json_object.end);
+  if (isNaN(end)){
+    res.sendStatus(400);
+    return;
+  }
+  
+  try {
+      //gives the dictionarey to my db functions
+      let results = await db.timecardhours(employeeId,start,end);
+      res.json(results);
+  } catch(e) {  
+    if (e.code = "ER_DUP_ENTRY"){
+      console.log(e);
+      res.sendStatus(400);
+      return;
+    }
+
+    console.log(e);
+    res.sendStatus(500);
+  }
+});*/
+
+
+
+//timecards   need joints  getemployeeid end start rang
 router.use('/documentation.html', swaggerUi.serve);
 router.get('/documentation.html', swaggerUi.setup(swaggerDocument));
 module.exports = router;
